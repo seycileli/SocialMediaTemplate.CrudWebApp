8b63128ca41e9f1484aae9446d15cd35f2dd7815
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-02-06 13:02:11 -0500

    Create EntityDao.java

---
+++ src_v1/src/main/java/dao/EntityDao.java
@@ -0,0 +1,26 @@
+package com.critix.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class EntityDao {
+
+    /**
+     * By doing this, we're creating a class of a reusable method
+     * that we can call when need be.
+     *
+     * This will allow us to simplify our application
+     * and make it more efficient, which we can
+     * call the Entity Manager whenever we need to read or write
+     * to the database.
+     *
+     * @param persistenceString
+     * @return
+     */
+    public static EntityManager getEntityManager(String persistenceString) {
+
+        return Persistence.createEntityManagerFactory(persistenceString)
+                        .createEntityManager();
+    }
+
+}
